2bec8c411f715a9b76ca756098d9761727b5b600
Ruslan <rus.galan@mail.ru>
2023-06-11 17:59:48 +0500

    Доделал работу с БД

--- app/src/main/java/com/example/smartdiet/Calc_Food.java
+++ app/src/main/java/com/example/smartdiet/Calc_Food.java
@@ -1,37 +1,28 @@
 package com.example.smartdiet;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.text.TextUtils;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 import com.example.smartdiet.Models.Eat;
-import com.example.smartdiet.Models.Weight;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.opencsv.CSVReader;
 
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -40,6 +31,7 @@ public class Calc_Food extends AppCompatActivity {
     public static String[] txtstr=new String[5];
     public static Integer[] tetstr=new Integer[5];
     public static Integer k=0;
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,7 +39,7 @@ public class Calc_Food extends AppCompatActivity {
         //Получение аргументов с прошлых layer
         Bundle arguments = getIntent().getExtras();
         //Формат даты
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
         //Сегодняшняя дата
         Date date=new Date();
         curDate=formatForDateNow.format(date);
@@ -57,29 +49,30 @@ public class Calc_Food extends AppCompatActivity {
         String[] kkal=new String[391];
         InputStream inputStream=getResources().openRawResource(R.raw.table);
         BufferedReader reader = new BufferedReader(
-                new InputStreamReader(inputStream, Charset.forName("UTF-8"))
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8)
         );
-        String line = "";
+        String line;
         try {
             reader.readLine();
-            Integer i=0;
+            int i=0;
             while ((line = reader.readLine()) != null) {
                 String[] tokens = line.split(";");
-                if (i==0)
-                    i++;
+                if (i==0) {
+                    continue;
+                }
                 else
                 {
                     name[i-1]=tokens[0];
                     kkal[i-1]=tokens[4];
-                    i++;
                 }
+                i++;
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
         BottomNavigationView bottomNavigationView = findViewById(R.id.btm_nav_view);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, name);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_singlechoice, name);
         AutoCompleteTextView txtcalc1=(AutoCompleteTextView)findViewById(R.id.txtcalc1);
         txtcalc1.setThreshold(1);
         txtcalc1.setAdapter(adapter);
@@ -102,61 +95,55 @@ public class Calc_Food extends AppCompatActivity {
         TextInputEditText tet5=findViewById(R.id.tet5);
         Double a=1.0;
         Button button=findViewById(R.id.button3);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Proverka(txtcalc1,tet1);
-                Proverka(txtcalc2,tet2);
-                Proverka(txtcalc3,tet3);
-                Proverka(txtcalc4,tet4);
-                Proverka(txtcalc5,tet5);
-                String itog="";
-                Double Kal=0.0;
-                for(int i=0;i<k;i++)
-                {
-                    itog=itog+";"+txtstr[i];
-                    Double Kaled=Double.parseDouble(kkal[ArrayUtils.indexOf(name,txtstr[i])])*(tetstr[i]*a/100);
-                    Kal=Kal+Kaled;
-                }
-                if(itog!="") {
-                    Eat eat = new Eat(arguments.get("log").toString(), itog, Kal, curDate);
-                    eatbd.push().setValue(eat);
-                }
+        button.setOnClickListener(v -> {
+            Proverka(txtcalc1,tet1);
+            Proverka(txtcalc2,tet2);
+            Proverka(txtcalc3,tet3);
+            Proverka(txtcalc4,tet4);
+            Proverka(txtcalc5,tet5);
+            StringBuilder itog= new StringBuilder();
+            double Kal=0.0;
+            for(int i=0;i<k;i++)
+            {
+                itog.append(txtstr[i]).append(";");
+                double Kaled=Double.parseDouble(kkal[ArrayUtils.indexOf(name,txtstr[i])])*(tetstr[i]*a/100);
+                Kal=Kal+Kaled;
+            }
+            if(!itog.toString().equals("")) {
+                Eat eat = new Eat(arguments.get("log").toString(), itog.toString(), Kal, curDate);
+                eatbd.push().setValue(eat);
             }
         });
 
         bottomNavigationView.getMenu().getItem(1).setChecked(true);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.change_weight:
-                        Intent intent = new Intent(Calc_Food.this, Change_Weight.class);
-                        intent.putExtra("log", arguments.get("log").toString());
-                        startActivity(intent);
-                        finish();
-                        return true;
-                    case R.id.delivery_food:
-                        Intent intent2 = new Intent(Calc_Food.this, Delivery_Food.class);
-                        intent2.putExtra("log", arguments.get("log").toString());
-                        startActivity(intent2);
-                        finish();
-                        return true;
-                    case R.id.about_app:
-                        Intent intent3 = new Intent(Calc_Food.this, About_App.class);
-                        intent3.putExtra("log", arguments.get("log").toString());
-                        startActivity(intent3);
-                        finish();
-                        return true;
-                    case R.id.person_app:
-                        Intent intent4 = new Intent(Calc_Food.this, Person_App.class);
-                        intent4.putExtra("log", arguments.get("log").toString());
-                        startActivity(intent4);
-                        finish();
-                        return true;
-                }
-                return false;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.change_weight:
+                    Intent intent = new Intent(Calc_Food.this, Change_Weight.class);
+                    intent.putExtra("log", arguments.get("log").toString());
+                    startActivity(intent);
+                    finish();
+                    return true;
+                case R.id.delivery_food:
+                    Intent intent2 = new Intent(Calc_Food.this, Delivery_Food.class);
+                    intent2.putExtra("log", arguments.get("log").toString());
+                    startActivity(intent2);
+                    finish();
+                    return true;
+                case R.id.about_app:
+                    Intent intent3 = new Intent(Calc_Food.this, About_App.class);
+                    intent3.putExtra("log", arguments.get("log").toString());
+                    startActivity(intent3);
+                    finish();
+                    return true;
+                case R.id.person_app:
+                    Intent intent4 = new Intent(Calc_Food.this, Person_App.class);
+                    intent4.putExtra("log", arguments.get("log").toString());
+                    startActivity(intent4);
+                    finish();
+                    return true;
             }
+            return false;
         });
     }
     public static void Proverka(AutoCompleteTextView str,TextInputEditText str1)
