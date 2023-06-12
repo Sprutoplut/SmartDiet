5bf09b8821a4b5f84034ee40975437f095097f89
Ruslan <rus.galan@mail.ru>
2023-06-13 00:10:04 +0500

    Карта

--- app/src/main/java/com/example/smartdiet/Delivery_Food.java
+++ app/src/main/java/com/example/smartdiet/Delivery_Food.java
@@ -2,28 +2,17 @@ package com.example.smartdiet;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Observable;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
-import com.google.android.gms.common.data.DataBufferObserver;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import com.yandex.mapkit.Animation;
-import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.location.FilteringMode;
@@ -32,34 +21,24 @@ import com.yandex.mapkit.location.LocationListener;
 import com.yandex.mapkit.location.LocationManager;
 import com.yandex.mapkit.location.LocationStatus;
 import com.yandex.mapkit.map.CameraPosition;
-
 import com.yandex.mapkit.map.PlacemarkMapObject;
 import com.yandex.mapkit.mapview.MapView;
-import com.yandex.mapkit.user_location.UserLocationLayer;
 import com.yandex.runtime.image.ImageProvider;
-import com.yandex.runtime.subscription.Subscription;
-
 import android.Manifest;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import pl.charmas.android.reactivelocation2.ReactiveLocationProvider;
-
 
 public class Delivery_Food extends AppCompatActivity implements RecyclerViewAdapter.ItemClickListener {
     RecyclerViewAdapter adapter;
     public static List<Point> mappoints = new ArrayList<>();
     private static Boolean initialized = false;
-    private PlacemarkMapObject Now_Geoposition;
-    private CoordinatorLayout rootCoordinatorLayout;
     private LocationManager locationManager;
     private LocationListener myLocationListener;
     private Point myLocation;
-    private static final double DESIRED_ACCURACY = 0;
-    private static final long MINIMAL_TIME = 1000;
-    private static final double MINIMAL_DISTANCE = 1;
-    private static final boolean USE_IN_BACKGROUND = false;
+    private PlacemarkMapObject Now_Geoposition;
     public void MapKitInitializer(Boolean initialized1) {
         if (initialized1) {
             return;
@@ -71,13 +50,14 @@ public class Delivery_Food extends AppCompatActivity implements RecyclerViewAdap
 
     private MapView mapView;
     private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 101;
+    @SuppressLint({"NonConstantResourceId", "ServiceCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MapKitInitializer(initialized);
         setContentView(R.layout.activity_delivery_food);
         Bundle arguments = getIntent().getExtras();
-        mapView = (MapView) findViewById(R.id.mapview);
+        mapView = findViewById(R.id.mapview);
         mapView.getMap().move(
                 new CameraPosition(new Point(54.740312, 55.986193), 13.0f, 0.0f, 0.0f),
                 new Animation(Animation.Type.SMOOTH, 0),
@@ -85,79 +65,58 @@ public class Delivery_Food extends AppCompatActivity implements RecyclerViewAdap
         ActivityCompat.requestPermissions(Delivery_Food.this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                 MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        ImageProvider imageprov = ImageProvider.fromResource(this, R.drawable.me);
-        Point mappoint = new Point(54.738662, 55.992206);
-        mapView.getMap().getMapObjects().addPlacemark(mappoint, ImageProvider.fromResource(this, R.drawable.maps_shop));
-        mappoints.add(mappoint);
-        mappoint = new Point(54.742256, 55.987142);
-        mapView.getMap().getMapObjects().addPlacemark(mappoint, ImageProvider.fromResource(this, R.drawable.maps_shop));
-        mappoints.add(mappoint);
-        mappoint = new Point(54.731640, 55.976641);
-        mapView.getMap().getMapObjects().addPlacemark(mappoint, ImageProvider.fromResource(this, R.drawable.maps_shop));
-        mappoints.add(mappoint);
-        mappoint = new Point(54.743546, 55.997750);
-        mapView.getMap().getMapObjects().addPlacemark(mappoint, ImageProvider.fromResource(this, R.drawable.maps_shop));
-        mappoints.add(mappoint);
-        mappoint = new Point(54.750547, 55.993280);
-        mapView.getMap().getMapObjects().addPlacemark(mappoint, ImageProvider.fromResource(this, R.drawable.maps_shop));
-        mappoints.add(mappoint);
-        mappoint = new Point(54.753281, 55.989139);
-        mapView.getMap().getMapObjects().addPlacemark(mappoint, ImageProvider.fromResource(this, R.drawable.maps_shop));
-        mappoints.add(mappoint);
-        mappoint = new Point(54.757280, 55.994103);
-        mapView.getMap().getMapObjects().addPlacemark(mappoint, ImageProvider.fromResource(this, R.drawable.maps_shop));
-        mappoints.add(mappoint);
-        mappoint = new Point(54.734680, 55.993218);
-        mapView.getMap().getMapObjects().addPlacemark(mappoint, ImageProvider.fromResource(this, R.drawable.maps_shop));
-        mappoints.add(mappoint);
-        mappoint = new Point(54.726603, 55.962610);
-        mapView.getMap().getMapObjects().addPlacemark(mappoint, ImageProvider.fromResource(this, R.drawable.maps_shop));
-        mappoints.add(mappoint);
-        ArrayList<String> MagazineName = new ArrayList<>();
-        MagazineName.add("Магнит 'Семейный'");
-        MagazineName.add("ВкуссВилл");
-        MagazineName.add("Пятёрочка");
-        MagazineName.add("Магнит");
-        MagazineName.add("Пятёрочка");
-        MagazineName.add("Пятёрочка");
-        MagazineName.add("Магнит");
-        MagazineName.add("Магнит");
-        MagazineName.add("Магнит");
-        ArrayList<String> MagazineName2 = new ArrayList<>();
-        MagazineName2.add("Комсомольская улица, 2к2");
-        MagazineName2.add("проспект Октября, 9");
-        MagazineName2.add("Революционная улица, 84");
-        MagazineName2.add("Комсомольская улица, 18/2");
-        MagazineName2.add("улица Братьев Кадомцевых, 8А");
-        MagazineName2.add("улица Рихарда Зорге, 21");
-        MagazineName2.add("улица Рихарда Зорге, 35");
-        MagazineName2.add("бульвар Хадии Давлетшиной, 24");
-        MagazineName2.add("улица Чернышевского, 125");
-        ArrayList<Integer> MyImage = new ArrayList<>();
-        MyImage.add(R.drawable.magnit);
-        MyImage.add(R.drawable.vkusvill);
-        MyImage.add(R.drawable.pyterochka);
-        MyImage.add(R.drawable.magnit);
-        MyImage.add(R.drawable.pyterochka);
-        MyImage.add(R.drawable.pyterochka);
-        MyImage.add(R.drawable.magnit);
-        MyImage.add(R.drawable.magnit);
-        MyImage.add(R.drawable.magnit);
-        MapKit mapKit = MapKitFactory.getInstance();
-        locationManager = MapKitFactory.getInstance().createLocationManager();
-        myLocationListener = new LocationListener() {
+        ImageProvider imageprov = ImageProvider.fromResource(this, R.drawable.maps_shop);
+        mappoints.add(new Point(54.738662, 55.992206));
+        mappoints.add(new Point(54.742256, 55.987142));
+        mappoints.add(new Point(54.731640, 55.976641));
+        mappoints.add(new Point(54.743546, 55.997750));
+        mappoints.add(new Point(54.750547, 55.993280));
+        mappoints.add(new Point(54.753281, 55.989139));
+        mappoints.add(new Point(54.757280, 55.994103));
+        mappoints.add(new Point(54.734680, 55.993218));
+        mappoints.add(new Point(54.726603, 55.962610));
+        for(int i=0;i<9;i++)
+        {
+            PointAdd(mappoints.get(i),imageprov);
+        }
+        ImageView minus=findViewById(R.id.minus);
+        minus.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onLocationUpdated(Location location) {
-                if (myLocation == null) {
-                    moveCamera(location.getPosition(), 15.0f);
-                }
-                myLocation = location.getPosition(); //this user point
+            public void onClick(View v) {
+                mapView.getMap().move(
+                        new CameraPosition(mapView.getMap().getCameraPosition().getTarget(), mapView.getMap().getCameraPosition().getZoom()-1.0f, 0.0f, 0.0f),
+                        new Animation(Animation.Type.SMOOTH, 0),
+                        null);
+            }
+        });
+        ImageView plus=findViewById(R.id.plus);
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mapView.getMap().move(
+                        new CameraPosition(mapView.getMap().getCameraPosition().getTarget(), mapView.getMap().getCameraPosition().getZoom()+1.0f, 0.0f, 0.0f),
+                        new Animation(Animation.Type.SMOOTH, 0),
+                        null);
+            }
+        });
+        ArrayList<String> MagazineName = new ArrayList<>(Arrays.asList("Магнит 'Семейный'","ВкуссВилл","Пятёрочка","Магнит","Пятёрочка","Пятёрочка","Магнит","Магнит","Магнит"));
+        ArrayList<String> MagazineName2 = new ArrayList<>(Arrays.asList("Комсомольская улица, 2к2","проспект Октября, 9","Революционная улица, 84","Комсомольская улица, 18/2","улица Братьев Кадомцевых, 8А","улица Рихарда Зорге, 21","улица Рихарда Зорге, 35","бульвар Хадии Давлетшиной, 24","улица Чернышевского, 125"));
+        ArrayList<Integer> MyImage = new ArrayList<>(Arrays.asList(R.drawable.magnit,R.drawable.vkusvill,R.drawable.pyterochka,R.drawable.magnit,R.drawable.pyterochka,R.drawable.pyterochka,R.drawable.magnit,R.drawable.magnit,R.drawable.magnit));
+        locationManager = MapKitFactory.getInstance().createLocationManager();
+        Now_Geoposition = mapView.getMap().getMapObjects().addPlacemark(new Point(54.749667, 55.998604), ImageProvider.fromResource(this, R.drawable.me));
+        myLocationListener=new LocationListener() {
+            @Override
+            public void onLocationUpdated(@NonNull Location location) {
+                myLocation = location.getPosition();
+                Now_Geoposition.setGeometry(new Point(myLocation.getLatitude(),myLocation.getLongitude()));
             }
 
             @Override
-            public void onLocationStatusUpdated(LocationStatus locationStatus) {
+            public void onLocationStatusUpdated(@NonNull LocationStatus locationStatus) {
+
             }
         };
+        locationManager.requestSingleUpdate(myLocationListener);
         RecyclerView recyclerView = findViewById(R.id.rvAnimals);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new RecyclerViewAdapter(this, MagazineName, MyImage,MagazineName2);
@@ -165,39 +124,40 @@ public class Delivery_Food extends AppCompatActivity implements RecyclerViewAdap
         recyclerView.setAdapter(adapter);
         BottomNavigationView bottomNavigationView = findViewById(R.id.btm_nav_view);
         bottomNavigationView.getMenu().getItem(2).setChecked(true);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.change_weight:
-                        Intent intent = new Intent(Delivery_Food.this, Change_Weight.class);
-                        intent.putExtra("log", arguments.get("log").toString());
-                        startActivity(intent);
-                        finish();
-                        return true;
-                    case R.id.calc_food:
-                        Intent intent1 = new Intent(Delivery_Food.this, Calc_Food.class);
-                        intent1.putExtra("log", arguments.get("log").toString());
-                        startActivity(intent1);
-                        finish();
-                        return true;
-                    case R.id.about_app:
-                        Intent intent3 = new Intent(Delivery_Food.this, About_App.class);
-                        intent3.putExtra("log", arguments.get("log").toString());
-                        startActivity(intent3);
-                        finish();
-                        return true;
-                    case R.id.person_app:
-                        Intent intent4 = new Intent(Delivery_Food.this, Person_App.class);
-                        intent4.putExtra("log", arguments.get("log").toString());
-                        startActivity(intent4);
-                        finish();
-                        return true;
-                }
-                return false;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.change_weight:
+                    Intent intent = new Intent(Delivery_Food.this, Change_Weight.class);
+                    intent.putExtra("log", arguments.get("log").toString());
+                    startActivity(intent);
+                    finish();
+                    return true;
+                case R.id.calc_food:
+                    Intent intent1 = new Intent(Delivery_Food.this, Calc_Food.class);
+                    intent1.putExtra("log", arguments.get("log").toString());
+                    startActivity(intent1);
+                    finish();
+                    return true;
+                case R.id.about_app:
+                    Intent intent3 = new Intent(Delivery_Food.this, About_App.class);
+                    intent3.putExtra("log", arguments.get("log").toString());
+                    startActivity(intent3);
+                    finish();
+                    return true;
+                case R.id.person_app:
+                    Intent intent4 = new Intent(Delivery_Food.this, Person_App.class);
+                    intent4.putExtra("log", arguments.get("log").toString());
+                    startActivity(intent4);
+                    finish();
+                    return true;
             }
+            return false;
         });
 
+    }
+    public void PointAdd(Point point,ImageProvider img)
+    {
+        mapView.getMap().getMapObjects().addPlacemark(point, img);
     }
     @Override
     public void onItemClick(View view, int position) {
@@ -211,6 +171,7 @@ public class Delivery_Food extends AppCompatActivity implements RecyclerViewAdap
         // Вызов onStop нужно передавать инстансам MapView и MapKit.
         mapView.onStop();
         MapKitFactory.getInstance().onStop();
+        locationManager.unsubscribe(myLocationListener);
         super.onStop();
     }
 
@@ -224,13 +185,7 @@ public class Delivery_Food extends AppCompatActivity implements RecyclerViewAdap
     }
     private void subscribeToLocationUpdate() {
         if (locationManager != null && myLocationListener != null) {
-            locationManager.subscribeForLocationUpdates(DESIRED_ACCURACY, MINIMAL_TIME, MINIMAL_DISTANCE, USE_IN_BACKGROUND, FilteringMode.OFF, myLocationListener);
+            locationManager.subscribeForLocationUpdates(0, 10, 1, false, FilteringMode.OFF, myLocationListener);
         }
-    }
-    private void moveCamera(Point point, float zoom) {
-        mapView.getMap().move(
-                new CameraPosition(point, zoom, 0.0f, 0.0f),
-                new Animation(Animation.Type.SMOOTH, 1),
-                null);
     }
 }
